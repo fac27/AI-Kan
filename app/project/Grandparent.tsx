bc18cb1bc498cb5b3ff1cad4eb05a22b72f7cc9b
simonryrie <simonryrie@gmail.com>
2023-06-30 16:58:07 +0100

    Adapted routes to Next 13

---
+++ app/project/Grandparent.tsx
@@ -0,0 +1,13 @@
+import {FC} from "react";
+
+const Grandparent: FC = () => {
+  return (
+    <div>
+      <label htmlFor="promptInput">I want to make a...</label>
+      <input type="text" id="promptInput" placeholder="Snake game in React" />
+      <button type="submit">Submit</button>
+    </div>
+  );
+};
+
+export default Grandparent;
