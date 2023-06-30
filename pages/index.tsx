239dde8f9755e5de7042249930a8c5806f4a7347
eliazzo <bethazz@hotmail.co.uk>
2023-06-30 12:33:56 +0100

    login incomplete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import AuthForm from '../auth/auth-form'
 
 const Home: React.FC = () => {
-  return <h1>Hello World</h1>;
+  return <>
+  <h1>Hello World</h1>
+  <AuthForm/>
+  </>
 };
 
 export default Home;
---
+++ pages/project.tsx
@@ -0,0 +1,5 @@
+const Project: React.FC = () => {
+  return <h1>Project page </h1>
+}
+
+export default Project
