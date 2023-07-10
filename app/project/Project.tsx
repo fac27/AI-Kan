e1390c420d3cb765b793ece6042014fadd94e899
eliazzo <bethazz@hotmail.co.uk>
2023-07-10 13:02:59 +0100

    redirect user to login / page when they try to login without inserting email

--- app/project/Project.tsx
+++ app/project/Project.tsx
@@ -85,7 +85,7 @@ export default function Project() {
             )}
           </>
         ))}
-      {width && <Logout />}
+      <Logout />
     </Xwrapper>
   )
 }
--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,27 +1,23 @@
-import Project from "./Project";
+import Project from "./Project"
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
 
-export async function AuthProject () {
+export async function AuthProject() {
   const supabase = createRouteHandlerClient({ cookies })
 
   // Check if we have a session
-    const {
-      data: { session },
-    } = await supabase.auth.getSession()
-  
-    if (session) {
-      return (
-      <Project/>
-      )
-    }
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
 
-    else {
-      return (
-        <h1>You are not logged in</h1>
-        // add a button that redirects to login
-      )
-    }
+  if (session) {
+    return <Project />
+  } else {
+    return (
+      redirect('/')
+    )
+  }
 }
 
 export default AuthProject
