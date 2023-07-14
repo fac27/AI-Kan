cfe07a330df1acf8f8283fa5383439f23d128778
Taha <tahafhassan@gmail.com>
2023-07-14 12:46:50 +0100

    style titlenode

--- app/project/components/TitleNode.tsx
+++ app/project/components/TitleNode.tsx
@@ -1,5 +1,6 @@
 import { useCallback } from "react"
 import { Handle, Position } from "reactflow"
+import { card } from "../../Styles/TailwindClasses"
 
 const handleStyle = { left: 10 }
 
@@ -10,18 +11,30 @@ const TitleNode = ({ data }) => {
 
   return (
     <>
-      <Handle type="target" position={Position.Top} />
-      <div>
-        <label htmlFor="text">Text:</label>
-        <input id="text" name="text" onChange={onChange} className="nodrag" />
+      <div
+        className={`mt-10 flex flex-col items-center justify-center ${card} bg-orange-50`}
+      >
+        <form className="flex flex-col gap-5 items-center justify-center p-4">
+          <label className="text-3xl" htmlFor="promptInput">
+            I want to make...
+          </label>
+          <div className="flex justify-between mt-2.5">
+            <input
+              type="text"
+              id="promptInput"
+              placeholder="...a dating app for llamas"
+              className="p-1.5 rounded border border-black"
+            />
+          </div>
+          <button
+            type="submit"
+            className={`border border-black bg-gray-50 p-1.5 rounded ml-5`}
+          >
+            Submit
+          </button>
+        </form>
       </div>
       <Handle type="source" position={Position.Bottom} id="a" />
-      <Handle
-        type="source"
-        position={Position.Bottom}
-        id="b"
-        style={handleStyle}
-      />
     </>
   )
 }
